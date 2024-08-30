145fdfaac928c0818569fb2b8bf8d285e20b74d7
jasmine lindberg <Jasmine.lindberg@elev.ga.ntig.se>
2024-08-30 13:58:03 +0200

    Första

---
+++ lukt-text/Program.cs
@@ -0,0 +1,25 @@
+﻿String name;
+String plats;
+String kändis;
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.Clear(); 
+
+Console.WriteLine("Skriv in ett djur");
+name = Console.ReadLine();
+ 
+// Console.WriteLine("Hello"  + name + " ");
+
+
+  Console.WriteLine("Skriv in en plats");
+ plats = Console.ReadLine();
+
+
+
+Console.WriteLine("Skriv in en kändis");
+kändis = Console.ReadLine();
+
+    Console.WriteLine($"Din {name} dog på {plats}. mördaren är {kändis} AHAHAAHAHAHAHAHHAAAHAHAAHAHAHA MIMIMIMIMI ");
+
+Console.ReadLine();
